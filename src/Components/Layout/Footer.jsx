66d3666ecf824ce31b991518a7ae9a8a4842ff81
rasoul707 <r.ahmadifar.1377@gmail.com>
2022-12-01 10:20:39 +0330

    add html parser to footer enamad

--- src/Components/Layout/Footer.jsx
+++ src/Components/Layout/Footer.jsx
@@ -12,6 +12,7 @@ import WhatsappSvg from "Assets/img/icons/whatsapp-white.svg";
 import TelegramSvg from "Assets/img/icons/telegram-white.svg";
 import { postRss } from "Services";
 import { toast } from "Utils/toast";
+import parse from 'html-react-parser';
 
 const useStyles = makeStyles(theme => ({
   footer_root: {
@@ -360,10 +361,7 @@ export default function Footer({ about, footerMenu }) {
                 </a>
               </div>
               <div>
-                {enamad}
-                {/* <a href={enamadUrl} target="_blank">
-                  <img src="/images/enamad.png" alt="" />
-                </a> */}
+                {parse(enamad)}
               </div>
             </div>
           </Grid>
